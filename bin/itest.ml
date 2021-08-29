77c39f600c28c94049c6c815f9909bdc03eb91cc
EmileRolley <erolley@tutamail.com>
2021-08-29 18:25:22 +0200

    feat(itest): udpdate

--- bin/itest.ml
+++ bin/itest.ml
@@ -8,6 +8,9 @@ let simple_cut ?(c = Color.black) p =
 let simple_filled_cut ?(area = `Anz) ?(c = Color.black) p =
   I.cut ~area p (I.const c)
 
+let filled_with_border ?(area = `Anz) ?(c = Color.black) p =
+  p |> simple_filled_cut ~area ~c |> I.blend (simple_cut p)
+
 (* PASS. (mod y-flip) *)
 let two_stroked_straight_lines =
   P.empty
@@ -57,12 +60,12 @@ let simple_stroked_square =
   |> P.rect (Box2.v (v 0.25 0.25) (v 0.5 0.5))
   |> simple_cut ~c:(Color.v 0.2 0.2 0.8 1.)
 
-(* PASS. (mod y-flip) *)
+(* FIXME: FAIL. (mod y-flip) *)
 let imbricated_stroked_squares =
   P.empty
   |> P.rect (Box2.v (v 0.25 0.25) (v 0.5 0.5))
   |> P.rect (Box2.v (v 0.3 0.3) (v 0.4 0.4))
-  |> simple_cut ~c:(Color.v 0.2 0.2 0.8 1.)
+  |> filled_with_border ~area:`Anz ~c:(Color.v 0.2 0.2 0.8 1.)
 
 (* PASS. (mod y-flip) *)
 let simple_filled_rect =
@@ -120,7 +123,7 @@ let filled_qbezier =
   P.empty
   |> P.qcurve (v 0.6 0.2) (v 0.5 0.5)
   |> P.close
-  |> simple_filled_cut ~area:`Aeo ~c:(Color.v 0.48 0.71 0.38 1.)
+  |> simple_filled_cut ~c:(Color.v 0.48 0.71 0.38 1.)
 
 (** PASS. (mod y-flip) *)
 let mult_filled_qbeziers =
@@ -137,19 +140,26 @@ let triangle =
   |> P.line (v 0.6 0.2)
   |> P.line (v 0.4 0.8)
   |> P.close
-  |> simple_filled_cut ~area:`Aeo ~c:(Color.v 0.48 0.71 0.38 1.)
+  |> simple_filled_cut ~area:`Anz ~c:(Color.v 0.48 0.71 0.38 1.)
 
 (** PASS. (mod y-flip) *)
 let poly1 =
-  let path =
-    P.empty
-    |> P.sub (v 0.2 0.2)
-    |> P.line (v 0.2 0.6)
-    |> P.line (v 0.3 0.4)
-    |> P.line (v 0.5 0.6)
-    |> P.line (v 0.8 0.2)
-    |> P.close
-  in
-  path
-  |> simple_filled_cut ~area:`Aeo ~c:(Color.v 0.48 0.71 0.38 1.)
-  |> I.blend (simple_cut path)
+  P.empty
+  |> P.sub (v 0.2 0.2)
+  |> P.line (v 0.2 0.6)
+  |> P.line (v 0.3 0.4)
+  |> P.line (v 0.5 0.6)
+  |> P.line (v 0.8 0.2)
+  |> P.close
+  |> filled_with_border ~c:(Color.v 0.48 0.71 0.38 1.)
+
+(** PASS. (mod y-flip) *)
+let star =
+  P.empty
+  |> P.sub (v 0.2 0.1)
+  |> P.line (v 0.3 0.7)
+  |> P.line (v 0.4 0.1)
+  |> P.line (v 0.1 0.5)
+  |> P.line (v 0.5 0.5)
+  |> P.close
+  |> filled_with_border ~area:`Aeo ~c:(Color.v 0.48 0.71 0.38 1.)
