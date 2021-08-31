43e16af7d4dda66253ed69efb837bf117520e1d6
EmileRolley <erolley@tutamail.com>
2021-08-31 12:54:30 +0200

    feat(itest): y-flip support

--- bin/itest.ml
+++ bin/itest.ml
@@ -11,7 +11,7 @@ let simple_filled_cut ?(area = `Anz) ?(c = Color.black) p =
 let filled_with_border ?(area = `Anz) ?(c = Color.black) p =
   p |> simple_filled_cut ~area ~c |> I.blend (simple_cut p)
 
-(* PASS. (mod y-flip) *)
+(* PASS. *)
 let two_stroked_straight_lines =
   P.empty
   |> P.sub (v 0.2 0.)
@@ -19,7 +19,7 @@ let two_stroked_straight_lines =
   |> P.line (v 0.25 0.5)
   |> simple_cut
 
-(* PASS. (mod y-flip) *)
+(* PASS. *)
 let three_stroked_straight_lines =
   P.empty
   |> P.sub (v 0.2 0.)
@@ -28,7 +28,7 @@ let three_stroked_straight_lines =
   |> P.line (v (-0.25) 0.3)
   |> simple_cut
 
-(* PASS. (mod y-flip) *)
+(* PASS. *)
 let two_stroked_sub_lines =
   P.empty
   |> P.sub (v 0.2 0.)
@@ -37,7 +37,7 @@ let two_stroked_sub_lines =
   |> P.line (v (-0.25) 0.3)
   |> simple_cut
 
-(* PASS. (mod y-flip) *)
+(* PASS. *)
 let closed_sub_paths =
   P.empty
   |> P.sub (v 0.2 0.)
@@ -48,39 +48,39 @@ let closed_sub_paths =
   |> P.close
   |> simple_cut
 
-(* PASS. (mod y-flip) *)
+(* PASS. *)
 let issue_12 =
   P.empty
   |> P.rect (Box2.v (v 0.25 0.25) (v 0.75 0.75))
   |> simple_cut ~c:(Color.v 0.2 0.2 0.8 1.)
 
-(* PASS. (mod y-flip) *)
+(* PASS. *)
 let simple_stroked_square =
   P.empty
   |> P.rect (Box2.v (v 0.25 0.25) (v 0.5 0.5))
   |> simple_cut ~c:(Color.v 0.2 0.2 0.8 1.)
 
-(* PASS. (mod y-flip) *)
+(* PASS. *)
 let imbricated_stroked_squares =
   P.empty
   |> P.rect (Box2.v (v 0.25 0.25) (v 0.5 0.5))
   |> P.rect (Box2.v (v 0.3 0.3) (v 0.4 0.4))
   |> filled_with_border ~area:`Anz ~c:(Color.v 0.2 0.2 0.8 1.)
 
-(* PASS. (mod y-flip) *)
+(* PASS. *)
 let simple_filled_rect =
   P.empty
   |> P.rect (Box2.v (v 0.25 0.25) (v 0.5 0.5))
   |> simple_filled_cut ~area:`Aeo ~c:(Color.v 0.88 0.69 1. 1.)
 
-(* PASS. (mod y-flip) *)
+(* PASS. *)
 let imbricated_filled_squares_same_dir =
   P.empty
   |> P.rect (Box2.v (v 0.25 0.25) (v 0.5 0.5))
   |> P.rect (Box2.v (v 0.3 0.3) (v 0.4 0.4))
   |> filled_with_border ~area:`Anz ~c:(Color.v 0.88 0.69 1. 1.)
 
-(* PASS. (mod y-flip) *)
+(* PASS. *)
 let imbricated_filled_squares_not_same_dir =
   P.empty
   |> P.rect (Box2.v (v 0.25 0.25) (v 0.5 0.5))
@@ -91,25 +91,25 @@ let imbricated_filled_squares_not_same_dir =
   |> P.close
   |> filled_with_border ~area:`Anz ~c:(Color.v 0.88 0.69 1. 1.)
 
-(** PASS. (mod y-flip) *)
+(** PASS. *)
 let basic_cbezier =
   P.empty |> P.ccurve (v 0.8 0.2) (v 0.2 0.8) (v 0.5 0.5) |> simple_cut
 
-(** PASS. (mod y-flip) *)
+(** PASS. *)
 let closed_cbezier =
   P.empty
   |> P.ccurve (v 0.8 0.2) (v 0.8 0.2) (v 0.5 0.5)
   |> P.close
   |> simple_cut
 
-(** PASS. (mod y-flip) *)
+(** PASS. *)
 let filled_cbezier =
   P.empty
   |> P.ccurve (v 0.8 0.2) (v 0.8 0.2) (v 0.5 0.5)
   |> P.close
   |> simple_filled_cut ~area:`Aeo ~c:(Color.v 0.48 0.71 0.38 1.)
 
-(** PASS. (mod y-flip) *)
+(** PASS. *)
 let mult_filled_cbeziers =
   P.empty
   |> P.ccurve (v 0.8 0.2) (v 0.8 0.2) (v 0.5 0.5)
@@ -117,7 +117,7 @@ let mult_filled_cbeziers =
   |> P.close
   |> simple_filled_cut ~area:`Aeo ~c:(Color.v 0.48 0.71 0.38 1.)
 
-(** PASS. (mod y-flip) *)
+(** PASS. *)
 let mult_filled_cbeziers_bug =
   P.empty
   |> P.ccurve (v 0.8 0.2) (v 0.8 0.2) (v 0.5 0.5)
@@ -125,18 +125,18 @@ let mult_filled_cbeziers_bug =
   |> P.close
   |> simple_filled_cut ~area:`Aeo ~c:(Color.v 0.48 0.71 0.38 1.)
 
-(** PASS. (mod y-flip) *)
+(** PASS. *)
 let simple_qbezier =
   P.empty |> P.qcurve (v 0.8 0.2) (v 0.5 0.5) |> P.close |> simple_cut
 
-(** PASS. (mod y-flip) *)
+(** PASS. *)
 let filled_qbezier =
   P.empty
   |> P.qcurve (v 0.6 0.2) (v 0.5 0.5)
   |> P.close
   |> simple_filled_cut ~c:(Color.v 0.48 0.71 0.38 1.)
 
-(** PASS. (mod y-flip) *)
+(** PASS. *)
 let mult_filled_qbeziers =
   P.empty
   |> P.qcurve (v 0.8 0.2) (v 0.5 0.5)
@@ -144,7 +144,7 @@ let mult_filled_qbeziers =
   |> P.close
   |> simple_filled_cut ~area:`Aeo ~c:(Color.v 0.48 0.71 0.38 1.)
 
-(** PASS. (mod y-flip) *)
+(** PASS. *)
 let triangle =
   P.empty
   |> P.sub (v 0.2 0.2)
@@ -153,7 +153,7 @@ let triangle =
   |> P.close
   |> simple_filled_cut ~area:`Anz ~c:(Color.v 0.48 0.71 0.38 1.)
 
-(** PASS. (mod y-flip) *)
+(** PASS. *)
 let poly1 =
   P.empty
   |> P.sub (v 0.2 0.2)
@@ -164,7 +164,7 @@ let poly1 =
   |> P.close
   |> filled_with_border ~area:`Aeo ~c:(Color.v 0.48 0.71 0.38 1.)
 
-(** PASS. (mod y-flip) *)
+(** PASS. *)
 let star =
   P.empty
   |> P.sub (v 0.2 0.1)
