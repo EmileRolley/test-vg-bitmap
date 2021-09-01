6d4411c14f4690775023913d7cc8765e1f2b8e5e
EmileRolley <erolley@tutamail.com>
2021-09-01 13:08:13 +0200

    feat(itest): add transformations

--- bin/itest.ml
+++ bin/itest.ml
@@ -176,13 +176,16 @@ let star =
   |> filled_with_border ~area:`Anz ~c:(Color.v 0.48 0.71 0.38 1.)
 
 (** PASS. *)
-let scaled_poly =
-  P.empty
-  |> P.sub (v 0.2 0.2)
-  |> P.line (v 0.2 0.6)
-  |> P.line (v 0.3 0.4)
-  |> P.line (v 0.5 0.6)
-  |> P.line (v 0.8 0.2)
-  |> P.close
-  |> filled_with_border ~area:`Aeo ~c:(Color.v 0.48 0.71 0.38 1.)
-  |> I.scale (v 0.5 0.5)
+let scale_poly = I.scale (v 0.5 0.5) poly1
+
+(** PASS. *)
+let move_poly = I.move (v 0.25 0.25) scale_poly
+
+(** PASS. *)
+let rot_poly = I.rot 0.20 move_poly
+
+(** PASS. *)
+let m_poly = I.tr M3.(v 0.5 0. 0.3 0. 1. 0. 0. 0. 1.) scale_poly
+
+(** PASS. *)
+let m_poly2 = I.tr M3.(v 0.5 0. 0.3 0. 1. 0. 0. 0. 1.) rot_poly
