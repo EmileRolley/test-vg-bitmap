d9020961245b84fa12180d189c31349e33626cb4
EmileRolley <erolley@tutamail.com>
2021-09-08 17:39:48 +0200

    refactor!: now generate all pairs of images in the imgs/ directory

--- bin/itest.ml
+++ bin/itest.ml
@@ -61,7 +61,7 @@ let simple_stroked_square =
   |> simple_cut ~c:(Color.v 0.2 0.2 0.8 1.)
 
 (* PASS. *)
-let imbricated_stroked_squares =
+let imbricated_squares =
   P.empty
   |> P.rect (Box2.v (v 0.25 0.25) (v 0.5 0.5))
   |> P.rect (Box2.v (v 0.3 0.3) (v 0.4 0.4))
@@ -164,7 +164,6 @@ let poly1 =
   |> P.close
   |> filled_with_border ~area:`Aeo ~c:(Color.v 0.48 0.71 0.38 1.)
 
-(** PASS. *)
 let star =
   P.empty
   |> P.sub (v 0.2 0.1)
@@ -173,19 +172,156 @@ let star =
   |> P.line (v 0.1 0.5)
   |> P.line (v 0.5 0.5)
   |> P.close
-  |> filled_with_border ~area:`Anz ~c:(Color.v 0.48 0.71 0.38 1.)
 
 (** PASS. *)
-let scale_poly = I.scale (v 0.5 0.5) poly1
+let nz_star =
+  star |> filled_with_border ~area:`Anz ~c:(Color.v 0.48 0.71 0.38 1.)
 
 (** PASS. *)
-let move_poly = I.move (v 0.25 0.25) scale_poly
+let eo_star =
+  star |> filled_with_border ~area:`Aeo ~c:(Color.v 0.48 0.71 0.38 1.)
 
 (** PASS. *)
-let rot_poly = I.rot 0.20 move_poly
+let scaled_poly = I.scale (v 0.5 0.5) poly1
 
 (** PASS. *)
-let m_poly = I.tr M3.(v 0.5 0. 0.3 0. 1. 0. 0. 0. 1.) scale_poly
+let moved_poly = I.move (v 0.25 0.25) scaled_poly
 
 (** PASS. *)
-let m_poly2 = I.tr M3.(v 0.5 0. 0.3 0. 1. 0. 0. 0. 1.) rot_poly
+let rotated_poly = I.rot 0.20 moved_poly
+
+(** PASS. *)
+let m_poly = I.tr M3.(v 0.5 0. 0.3 0. 1. 0. 0. 0. 1.) scaled_poly
+
+(** PASS. *)
+let m_poly2 = I.tr M3.(v 0.5 0. 0.3 0. 1. 0. 0. 0. 1.) rotated_poly
+
+(* FAIL. *)
+let even_odd_winding_area_rule =
+  let directed_pentagram arrow area r =
+    let arrow p0 p1 =
+      (* arrow at the beginning of the line p0p1. *)
+      let l = V2.(p1 - p0) in
+      let angle = V2.angle l in
+      let loc = V2.(p0 + (0.2 * l)) in
+      I.const Color.black |> I.cut arrow |> I.rot angle |> I.move loc
+    in
+    let a = Float.pi_div_2 in
+    (* points of the pentagram. *)
+    let da = Float.two_pi /. 5. in
+    let p0 = V2.polar r a in
+    let p1 = V2.polar r (a -. (2. *. da)) in
+    let p2 = V2.polar r (a +. da) in
+    let p3 = V2.polar r (a -. da) in
+    let p4 = V2.polar r (a +. (2. *. da)) in
+    let pentagram =
+      (* http://mathworld.wolfram.com/StarPolygon.html *)
+      P.(empty |> sub p0 |> line p1 |> line p2 |> line p3 |> line p4 |> close)
+    in
+    let lines = `O { P.o with P.width = 0.01 } in
+    I.const (Color.gray 0.8)
+    |> I.cut ~area pentagram
+    |> I.blend (I.const Color.black |> I.cut ~area:lines pentagram)
+    |> I.blend (arrow p0 p1)
+    |> I.blend (arrow p1 p2)
+    |> I.blend (arrow p2 p3)
+    |> I.blend (arrow p3 p4)
+    |> I.blend (arrow p4 p0)
+  in
+
+  let directed_annulus arrow ~rev area r =
+    let arrow ?(rev = false) r a =
+      (* arrow at polar coordinate (r, a). *)
+      let angle = a +. if rev then -.Float.pi_div_2 else Float.pi_div_2 in
+      let loc = V2.polar r a in
+      I.const Color.black |> I.cut arrow |> I.rot angle |> I.move loc
+    in
+    let _arrows ?(rev = false) r =
+      arrow ~rev r 0.
+      |> I.blend (arrow ~rev r Float.pi_div_2)
+      |> I.blend (arrow ~rev r (2. *. Float.pi_div_2))
+      |> I.blend (arrow ~rev r (-.Float.pi_div_2))
+    in
+    let circle ?(rev = false) r =
+      let c = P.empty |> P.circle P2.o r in
+      if rev then (* flip *) P.tr (M3.scale2 (V2.v (-1.) 1.)) c else c
+    in
+    let outer = r in
+    let inner = r *. 0.6 in
+    let annulus = P.append (circle outer) (circle ~rev inner) in
+    let outline = `O { P.o with P.width = 0.01 } in
+    I.const (Color.gray 0.8)
+    |> I.cut ~area annulus
+    |> I.blend (I.const Color.black |> I.cut ~area:outline annulus)
+    (* |> I.blend (arrows outer) *)
+    (* |> I.blend (arrows ~rev inner) *)
+  in
+
+  let area_rule_examples area =
+    let arrow =
+      let a = Float.two_pi /. 3. in
+      let pt a = V2.polar 0.032 a in
+      P.(empty |> sub (pt 0.) |> line (pt (-.a)) |> line (pt a) |> close)
+    in
+    let pentagram = directed_pentagram arrow area 0.4 in
+    let annulus = directed_annulus arrow ~rev:false area 0.3 in
+    let annulus_r = directed_annulus arrow ~rev:true area 0.3 in
+    let y = 0.46 in
+    pentagram
+    |> I.move (V2.v 0.5 y)
+    |> I.blend (annulus |> I.move (V2.v 1.5 y))
+    |> I.blend (annulus_r |> I.move (V2.v 2.5 y))
+  in
+  area_rule_examples `Aeo
+
+(* PASS. *)
+let arrowhead_path =
+  let arrowhead_path i len =
+    let angle = Float.pi /. 3. in
+    let rec loop i len sign turn p =
+      if i = 0 then p |> P.line ~rel:true V2.(polar len turn)
+      else
+        p
+        |> loop (i - 1) (len /. 2.) (-.sign) (turn +. (sign *. angle))
+        |> loop (i - 1) (len /. 2.) sign turn
+        |> loop (i - 1) (len /. 2.) (-.sign) (turn -. (sign *. angle))
+    in
+    P.empty |> loop i len 1. 0.
+  in
+  let area = `O { P.o with P.width = 0.005 } in
+  let gray = I.const (Color.gray 0.1) in
+  let acc = ref I.void in
+  for i = 0 to 9 do
+    let x = float (i mod 2) +. 0.1 in
+    let y = (0.85 *. float (i / 2)) +. 0.1 in
+    acc :=
+      gray
+      |> I.cut ~area (arrowhead_path i 0.8)
+      |> I.move (V2.v x y)
+      |> I.blend !acc
+  done;
+  !acc
+
+let circle = P.empty |> P.circle (P2.v 0.5 0.5) 0.25 |> simple_cut
+
+let vg_imgs =
+  [|
+    ("two_stroked_straight_lines", two_stroked_straight_lines);
+    ("closed_sub_paths", closed_sub_paths);
+    ( "imbricated_filled_squares_not_same_dir",
+      imbricated_filled_squares_not_same_dir );
+    ("imbricated_filled_squares_same_dir", imbricated_filled_squares_same_dir);
+    ("closed_cbezier", closed_cbezier);
+    ("filled_cbezier", filled_cbezier);
+    ("mult_filled_cbeziers", mult_filled_cbeziers);
+    ("simple_qbezier", simple_qbezier);
+    ("non_zero_rule_star", nz_star);
+    ("even_odd_rule_star", eo_star);
+    ("poly1", poly1);
+    ("scaled_poly", scaled_poly);
+    ("moved_poly", moved_poly);
+    ("rotated_poly", rotated_poly);
+    ("tr_matrix_poly", m_poly);
+  |]
+
+(* |> filled_with_border ~area:`Anz ~c:(Color.v 0.48 0.71 0.38 1.) *)
--- bin/main.ml
+++ bin/main.ml
@@ -1,37 +1,8 @@
-open Landmark
 open Bimage
 open Gg
 open Vg
 module B = Vgr_bitmap.F32_ba
 module Bitmap_renderer = Vgr_bitmap.Make (B)
-
-(* To PNG functions (from school project). *)
-
-(*(1** Converts a Graphics.color [c] into its rgb code. *1) *)
-(*let color_to_rgb (rgb : float) = *)
-(*  (1* let rgb = Int32.to_int rgb in *1) *)
-(*  (1* Printf.printf "rgb: %dd\n" rgb; *1) *)
-(*  let rgb = Int.of_float rgb in *)
-(*  let r = (rgb lsr 16) land 0xFF in *)
-(*  (1* Printf.printf "r: %d\n" r; *1) *)
-(*  let g = (rgb lsr 8) land 0xFF in *)
-(*  (1* Printf.printf "g: %d\n" g; *1) *)
-(*  let b = rgb land 0xFF in *)
-(*  (1* Printf.printf "b: %d\n" b; *1) *)
-(*  (r, g, b) *)
-
-(*let to_255_int_rgb v = Int.of_float (255. *. v) *)
-
-(*let rgba_to_int rgb = *)
-(*  let open Gg.V4 in *)
-(*  let sa res = Stdlib.( + ) (res lsl 8) in *)
-(*  let res = x rgb |> to_255_int_rgb in *)
-(*  Printf.printf "res1: %d\n" res; *)
-(*  let res = y rgb |> to_255_int_rgb |> sa res in *)
-(*  Printf.printf "res2: %d\n" res; *)
-(*  let res = z rgb |> to_255_int_rgb |> sa res in *)
-(*  Printf.printf "res3: %d\n" res; *)
-(*  res *)
 
 (** Colors the pixel at ([x], [y]) of [img] with the color [c] *)
 let color_pixel img x y c =
@@ -52,8 +23,7 @@ let save path bitmap =
          let y' = Int.to_float y in
          color_pixel img x y (B.get bitmap x' y'))
        img);
-  Bimage_unix.Magick.write path img;
-  Printf.printf "PNG file saved here: %s\n" path
+  Bimage_unix.Magick.write path img
 
 let get_render_time yield =
   let s = Unix.gettimeofday () in
@@ -62,9 +32,8 @@ let get_render_time yield =
   e -. s
 
 let log_rendering name yield =
-  Printf.printf "\n[LOG] - Rendering %s image\n" name;
   let r_time = yield () in
-  Printf.printf "[LOG] - Done in %fs\n" r_time
+  Printf.printf "[LOG] - Rendered '%s' in %fs\n" name r_time
 
 let render_png_cairo file view size img =
   let res = 300. /. 0.0254 in
@@ -80,14 +49,11 @@ let render_png_cairo file view size img =
   close_out oc;
   r_time
 
-let render = register "render"
-
 let render_bitmap file view size img =
   let res = 300. /. 25.4 in
   let w = int_of_float (res *. Size2.w size) in
   let h = int_of_float (res *. Size2.h size) in
 
-  Printf.printf "w: %d, h: %d\n" w h;
   let bitmap = B.create w h in
   let target = Bitmap_renderer.target bitmap res in
   let warn w = Vgr.pp_warning Format.err_formatter w in
@@ -102,10 +68,22 @@ let render_bitmap file view size img =
 
 let () =
   let aspect = 1. in
-  let size = Size2.v (aspect *. 10.) 10. (* mm *) in
-  let view = Box2.v P2.o (Size2.v aspect 1.) in
-  let image = Itest.two_stroked_straight_lines in
-  log_rendering "cairo" (fun _ -> render_png_cairo "cairo.png" view size image);
-  log_rendering "bitmap" (fun _ -> render_bitmap "bitmap.png" view size image)
-
-(* ignore (render_bitmap "bitmap.png" view size image) *)
+  let size = Size2.v (aspect *. 100.) 100. (* mm *) in
+  let view = Box2.v P2.o (Size2.v (aspect *. 2.) 1.) in
+  let render r i name img =
+    let t, f =
+      match r with
+      | `Cairo -> ("cairo", render_png_cairo)
+      | `Bitmap -> ("bitmap", render_bitmap)
+    in
+    log_rendering
+      (t ^ "-" ^ name)
+      (fun _ ->
+        f
+          ("imgs/" ^ string_of_int i ^ "-" ^ t ^ "-" ^ name ^ ".png")
+          view size img)
+  in
+  Itest.vg_imgs
+  |> Array.iteri ~f:(fun i (name, img) ->
+         render `Cairo i name img;
+         render `Bitmap i name img)
